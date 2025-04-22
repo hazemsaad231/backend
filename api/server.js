64318506d4f2efa7d36c613d18764086817a0d95
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-04-22 22:01:45 +0200

    good

--- api/server.js
+++ api/server.js
@@ -1,3 +1,19 @@
+// const jsonServer = require('json-server');
+// const cors = require('cors');
+
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+
+// server.use(cors());
+// server.use(middlewares);
+// server.use(router);
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });
+
 const jsonServer = require('json-server');
 const cors = require('cors');
 
@@ -9,7 +25,7 @@ server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// تصدير الوظيفة للعمل مع Vercel
+module.exports = (req, res) => {
+  server(req, res);  // هذا يقوم بتشغيل السيرفر في بيئة غير متزامنة
+};
